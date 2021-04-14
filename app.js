0da689f0d7026b318e1f571cafea227ffaffbb76
platiniradukunda <platinirad@gmail.com>
2021-04-14 15:09:38 -0400

    start of html and style

--- app.js
+++ app.js
@@ -3,21 +3,20 @@ console.log(gridContainers);
 
 const player1 = "X"; // creating player 1 as X
 const player2 = "O"; // creating player 2 as O
-var player1Turn;    // declaring my variable for player 1's turn
-var player2Turn;    // declaring my variable for player 2's turn
+var turn = true;    // declaring a turn variable so that players can take turns
 
 // creating the function that will be triggered any time you click a grid
 var playerOption = (grid) => {
     // target the grid that triggered the function(The one that was clicked). That grid is now called box
     var box = grid.target;
     // fill grid with X or O
-    if(player1Turn = true){
+    if(turn == true){
         box.innerText = player1;  // modifying box accordingly
     } else{
         box.innerText = player2;
     }
-    // Taking turns between player 1 and player 2
-    takeTurns();
+    // Taking turns between player 1 and player 2. Starts off as player 1(x)
+    turn = !turn;
     // determine a winner
     
 }
@@ -28,11 +27,9 @@ gridContainers.forEach(grid => {
 })
 
 
-// a function to take turns between player 1 and player 2
 
-var takeTurns = () => {
-    player1Turn = !player1Turn
-}
+
+
 
 
 
